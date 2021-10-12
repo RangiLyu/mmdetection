5ec66eda1791280b724fa3ce3444e717e1d52278
RangiLyu <lyuchqi@gmail.com>
2021-10-12 11:08:37 +0800

    Add nanodet

--- configs/nanodet/nanodet_shufflenetv2-1.0_pan-lite_mstrain-416-yolo-xaug_280e_coco.py
+++ configs/nanodet/nanodet_shufflenetv2-1.0_pan-lite_mstrain-416-yolo-xaug_280e_coco.py
@@ -54,28 +54,27 @@ test_pipeline = [
         ])
 ]
 
-train_dataset = dict(
-    type='MultiImageMixDataset',
-    dataset=dict(
-        type=dataset_type,
-        ann_file=data_root + 'annotations/instances_train2017.json',
-        img_prefix=data_root + 'train2017/',
-        pipeline=[
-            dict(
-                type='LoadImageFromFile',
-                to_float32=True,
-                file_client_args=file_client_args),
-            dict(type='LoadAnnotations', with_bbox=True)
-        ],
-        filter_empty_gt=False,
-    ),
-    pipeline=train_pipeline,
-    dynamic_scale=img_scale)
-
 data = dict(
     samples_per_gpu=48,
     workers_per_gpu=8,
-    train=train_dataset,
+    train=dict(
+        _delete_=True,
+        type='MultiImageMixDataset',
+        dataset=dict(
+            type=dataset_type,
+            ann_file=data_root + 'annotations/instances_train2017.json',
+            img_prefix=data_root + 'train2017/',
+            pipeline=[
+                dict(
+                    type='LoadImageFromFile',
+                    to_float32=True,
+                    file_client_args=file_client_args),
+                dict(type='LoadAnnotations', with_bbox=True)
+            ],
+            filter_empty_gt=False,
+        ),
+        pipeline=train_pipeline,
+        dynamic_scale=img_scale),
     val=dict(
         type=dataset_type,
         ann_file=data_root + 'annotations/instances_val2017.json',
